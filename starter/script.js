3cc8ea7d5fc7dfca87ca310c9b2dc687af175841
siddharth raghav <thakurraghav27@gmail.com>
2022-07-18 17:47:08 +0530

    add esc

--- starter/script.js
+++ starter/script.js
@@ -22,7 +22,8 @@ for (let i = 0; i < btnsOpenModal.length; i++) {
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
 
-// document.body.addEventListener('click', function () {
-//   modal.classList.add('hidden');
-//   overlay.classList.add('hidden');
-// });
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
